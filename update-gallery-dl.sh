82d8e944a009bec06bcdfc70d4b7ae0cace059f6
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2025-12-13 23:42:51 -0600

    Updates

--- update-gallery-dl.sh
+++ update-gallery-dl.sh
@@ -13,5 +13,6 @@ python -m venv ~/.local/lib/"${app}"
 
 #~/.local/lib/"${app}"/bin/python -m pip install git+https://github.com/mikf/"${app}".git
 ~/.local/lib/"${app}"/bin/python -m pip install "${app}[extra]"
+~/.local/lib/"${app}"/bin/python -m pip install "yt-dlp[default,curl-cffi]"
 
 cp ~/.local/lib/"${app}"/bin/"${app}" ~/.local/bin/
--- update-yt-dlp.sh
+++ update-yt-dlp.sh
@@ -13,5 +13,6 @@ python -m venv ~/.local/lib/"${app}"
 
 #~/.local/lib/yt-dlp/bin/python -m pip install git+https://github.com/"${app}"/"${app}".git
 ~/.local/lib/"${app}"/bin/python -m pip install "${app}"[default,curl-cffi,secretstorage]
+~/.local/lib/"${app}"/bin/python -m pip install yt-dlp-ejs
 
 cp ~/.local/lib/"${app}"/bin/"${app}" ~/.local/bin/
